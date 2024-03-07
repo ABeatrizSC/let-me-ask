cb9390fd9977951f3c11b2b7577053c9d2852b64
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-03-06 22:49:12 -0300

    create home component and global style

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx';
 
 import './services/firebase.ts';
 
+import './styles/global.scss';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+export const Home = () => {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&A ao vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Logo do Let me Ask" />
+                    <button className="button-create-room">
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div className='separator'>Ou entre em uma sala</div>
+                    <form action="">
+                        <input 
+                            type="text" 
+                            placeholder='Digite o código da sala'
+                        />
+                        <Button type='submit'>
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
+  
